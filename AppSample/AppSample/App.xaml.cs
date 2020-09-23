701013a6d56a8d69773723cd0a781e578825461f
gabriell <gabriel@smit.net.br>
2020-09-22 21:33:37 -0300

    First version

---
+++ AppSample/AppSample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using AppSample.Services;
+
+namespace AppSample
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockItemDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
